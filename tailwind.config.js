7d89ddd7382153122dfbbb3240b638bd1d722005
chandanpolai <chandanpolai26@gmail.com>
2024-02-16 12:22:27 +0530

    intial-commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+// @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'open': ["Open Sans", 'sans-serif'],
+        'robot': ['Roboto', 'sans-serif'],
+        'Desgin': ["Protest Strike", 'sans-serif'],
+        'Poppiens': ["Poppins", 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
